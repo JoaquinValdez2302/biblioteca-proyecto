9bf0b64c4ad573b593bcece7832283a0b1f9fd70
JoaquinValdez <joaquinr.val@gmail.com>
2025-10-27 19:31:56 -0300

    deploy render, pt2

--- backend/src/services/socioService.js
+++ backend/src/services/socioService.js
@@ -1,5 +1,5 @@
 const socioRepository = require("../repositories/socioRepository");
-const { faker } = require("@faker-js/faker"); // Para generar el número de socio
+
 
 const registrarNuevoSocio = async (nombreCompleto, dni, email, telefono) => {
   // Ya no generamos el número aquí, el repositorio se encarga
